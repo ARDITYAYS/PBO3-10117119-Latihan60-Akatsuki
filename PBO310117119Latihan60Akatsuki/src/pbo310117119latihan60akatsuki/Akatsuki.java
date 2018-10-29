d97b252735056bba6c9a01b46dfc7ce1c0335667
Arditya Yogaputra S <43446706+ARDITYAYS@users.noreply.github.com>
2018-10-30 00:37:47 +0700

    Add files via upload

---
+++ PBO310117119Latihan60Akatsuki/src/pbo310117119latihan60akatsuki/Akatsuki.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo310117119latihan60akatsuki;
+
+/**
+ *
+ * @author Yogaputra
+ */
+public class Akatsuki {
+
+    protected String pakaian;
+    protected String tujuan;
+
+    public Akatsuki(String pakaian, String tujuan) {
+        this.pakaian = pakaian;
+        this.tujuan = tujuan;
+    }
+
+    public String getPakaian() {
+        return pakaian;
+    }
+
+    public String getTujuan() {
+        return tujuan;
+    }
+
+}
